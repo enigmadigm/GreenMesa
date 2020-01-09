9ca1e2707d0f4cf49f10df528ffb71d0c431c4d0
GalaxySH <polyversialmind@gmail.com>
2020-01-09 16:44:05 -0600

    minor bug fixes for uptime command

--- commands/uptime.js
+++ commands/uptime.js
@@ -13,7 +13,7 @@ module.exports = {
 
         let allSeconds = (client.uptime / 1000);
         let days = Math.floor(allSeconds / 86400);
-        let hours = Math.floor(allSeconds / 3600);
+        let hours = Math.floor((allSeconds / 3600) - (days * 24));
         allSeconds %= 3600;
         let minutes = Math.floor(allSeconds / 60);
         let seconds = Math.floor(allSeconds % 60);
