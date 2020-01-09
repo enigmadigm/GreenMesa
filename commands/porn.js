6e117c98bbe271590725cf47d194ec4d053f7e97
GalaxySH <polyversialmind@gmail.com>
2020-01-09 17:05:51 -0600

    NSFW removal for now, will remain in history

--- commands/porn.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'porn',
-    description: 'wut',
-    execute(client, message, args) {
-        message.channel.send("mmmmm no...not gonna make it *that* easy");
-    }
-}
