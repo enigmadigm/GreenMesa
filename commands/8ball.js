30fa2d97c70a32aaccff767cf3937543d028c798
GalaxySH <polyversialmind@gmail.com>
2020-01-11 13:20:37 -0600

    8ball will no longer confuse, it's magic 8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,12 +1,12 @@
 // 8ball response generator
 function doMagic8BallVoodoo() {
-    var rand = ['Yes', 'No', 'Why are you even trying?', 'What do you think? NO', 'Maybe', 'Never', 'Yep'];
+    var rand = ['Yes', 'No', 'Why try?', 'Maybe', 'Never'];
     return rand[Math.floor(Math.random() * rand.length)];
 }
 
 module.exports = {
     name: '8ball',
-    description: 'the game of 8ball: yes or no',
+    description: 'play some *magic* 8ball (not pool)',
     execute(client, message, args, conn) {
         message.channel.send(doMagic8BallVoodoo()).catch(console.error);
     }
