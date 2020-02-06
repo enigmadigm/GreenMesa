aa3571175f58c37ffcd3e3aa0234b4631b962cc4
Stefan Hauge <polyversialmind@gmail.com>
2020-02-05 23:11:12 -0600

    Real graphing feature added, more to come (plotly)

--- commands/botstats.js
+++ commands/botstats.js
@@ -1,4 +1,16 @@
-const config = require("../auth.json")
+const config = require("../auth.json");
+// const d3 = require("d3");
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+const fs = require('fs');
+const plotly = require('plotly')("GalaxySH", "JcEDtj2qJRq4vrU7dUvH")
+// const { saveSvgAsPng } = require("save-svg-as-png");
+// const sharp = require('sharp');
+
+// const document = jsdom.jsdom(), svg = d3.select(document.body).append("svg");
+// const document = new JSDOM(), svg = d3.select(document.body).append("svg");
+
+const Discord = require('discord.js');
 
 module.exports = {
     name: 'botstats',
@@ -8,59 +20,76 @@ module.exports = {
     async execute(client, message, args, conn) {
         // const statRows = await conn.query(`SELECT * FROM gmstatsBETA ORDER BY updateId DESC LIMIT 20`);
         conn.query(`SELECT * FROM gmstats ORDER BY updateId DESC LIMIT 24`, (err, rows) => {
-            let diffs = [];
-            for (let i = 0; i < rows.length; i++) {
-                if (i != rows.length - 1) {
-                    diffs.push(rows[i].numUsers - rows[i + 1].numUsers);
-                    // console.log(`${rows[i].numUsers} - ${rows[i + 1].numUsers}`);
-                }
-            }
-            let totalUserChange = 0;
-            diffs.forEach(e => {
-                totalUserChange += e;
-            });
-            avgUserChange = Math.floor(totalUserChange / diffs.length);
-            let chngPrefix = '+';
-            if (avgUserChange <= 0) {
-                chngPrefix = '';
-            }
-            message.channel.send({
-                embed: {
-                    title: "GreenMesa Stats",
-                    // "description": "Current statistics for this bot."
-                    fields: [
-                        {
-                            name: 'Total Servers',
-                            value: rows[0].numGuilds,
-                            inline: true
-                        },
-                        {
-                            name: 'Total Channels',
-                            value: rows[0].numChannels,
-                            inline: true
-                        },
-                        {
-                            name: 'Total Users',
-                            value: rows[0].numUsers,
-                            inline: true
-                        },
-                        {
-                            name: "Avg. user change over last 24 hr",
-                            value: chngPrefix+avgUserChange
-                        },
-                        {
-                            name: "Words Defined",
-                            value: config.wordsDefined.length
-                        }
-                    ],
-                    footer: {
-                        text: "Working on graphing"
+            data = [{ x: [], y: [], type: 'line' }];
+            conn.query(`SELECT * FROM gmstats ORDER BY updateId DESC LIMIT 24`, async (err, rows) => {
+                var layout = { format: 'png' };
+                rows.forEach((r, i) => {
+                    data[0].x.push(r.logDate);
+                    data[0].y.push(r.numUsers);
+                });
+                await plotly.getImage({ 'data': data }, layout, (err, imageData) => {
+                    if (err) return console.log(err);
+                    var fileStream = fs.createWriteStream('./media/usernumber-graph.png');
+                    imageData.pipe(fileStream);
+                });
+
+                /*==+==+==+==+==+==*+*==*+*==+==+==+==+==*+*==*+*==+==+==+==+==*+*==*+*==+==+==+==+==+==*/
+
+                let diffs = [];
+                for (let i = 0; i < rows.length; i++) {
+                    if (i != rows.length - 1) {
+                        diffs.push(rows[i].numUsers - rows[i + 1].numUsers);
+                        // console.log(`${rows[i].numUsers} - ${rows[i + 1].numUsers}`);
                     }
                 }
-            })
-        });
-        // if (queryStatus == "NO ROWS") {
-        //     return message.channel.send("The stats database must populate more before the stats command can be used, sorry.");
-        // }
+                let totalUserChange = 0;
+                diffs.forEach(e => {
+                    totalUserChange += e;
+                });
+                avgUserChange = Math.floor(totalUserChange / diffs.length);
+                let chngPrefix = '+';
+                if (avgUserChange <= 0) {
+                    chngPrefix = '';
+                }
+                message.channel.send({
+                    embed: {
+                        title: "GreenMesa Stats",
+                        // "description": "Current statistics for this bot."
+                        fields: [
+                            {
+                                name: 'Total Servers',
+                                value: rows[0].numGuilds,
+                                inline: true
+                            },
+                            {
+                                name: 'Total Channels',
+                                value: rows[0].numChannels,
+                                inline: true
+                            },
+                            {
+                                name: 'Total Users',
+                                value: rows[0].numUsers,
+                                inline: true
+                            },
+                            {
+                                name: "Avg. user change over last 24 hr",
+                                value: chngPrefix + avgUserChange
+                            },
+                            {
+                                name: "Words Defined",
+                                value: config.wordsDefined.length
+                            }
+                        ],
+                        footer: {
+                            text: "Working on graphing"
+                        }
+                    },
+                    // files: ['./media/usernumber-graph.png']
+                })
+                    .then(message.channel.send({ files: ["./media/usernumber-graph.png"] }))
+                    .catch(console.error)
+                
+            });//second query
+        });//first query
     }
 }
