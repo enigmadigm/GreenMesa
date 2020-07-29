0324dcaa65bfd7bfb95e77bbbbdf6c5bf32ed715
PM <polyversialmind@gmail.com>
2020-07-29 05:20:22 -0500

    testing new methods for plotly and new embed setup

--- commands/botstats.js
+++ commands/botstats.js
@@ -4,37 +4,46 @@ const config = require("../auth.json");
 // const { JSDOM } = jsdom;
 const fs = require('fs');
 const plotly = require('plotly')(config.plotly.username, config.plotly.key)
+const Discord = require("discord.js");
+
+function generatePlot(rrows) {
+    return new Promise((resolve, reject) => {
+        var data = {
+            type: 'line',
+            x: [],
+            y: []
+        };
+        rrows.forEach((r) => {
+            data.x.push(r.logDate);
+            data.y.push(r.numUsers);
+        });
+        var pngOpts = {
+            format: 'png',
+            height: 370,
+            width: 520,
+        };
+        var layout = {
+            plot_bgcolor: 'rgb(52, 54, 60)',
+            paper_bgcolor: 'rgb(52, 54, 60)',
+        };
+        var chart = { 'data': [data], 'layout': layout }
+        plotly.getImage(chart, pngOpts, (err, imageData) => {
+            if (err) return console.log(err);
+            var fileStream = fs.createWriteStream('./media/usernumber-graph.png');
+            imageData.pipe(fileStream);
+            fileStream.on('error', reject);
+            fileStream.on('finish', resolve);
+        });
+    });
+}
 
 module.exports = {
     name: 'botstats',
     aliases: ['bs'],
-    description: 'An assortment of GreenMesa stats. **In development**',
+    description: 'Some statistics for this bot.',
     async execute(client, message, args, conn) {
-        var data = [{
-            type: 'line',
-            x: [],
-            y: []
-        }];
         conn.query(`SELECT * FROM gmstats ORDER BY updateId DESC LIMIT 24`, async (err, rows) => {
-            rows.forEach((r) => {
-                data[0].x.push(r.logDate);
-                data[0].y.push(r.numUsers);
-            });
-            var layout = {
-                format: 'png',
-                height: 370,
-                width: 520,
-            };
-            try {
-                await plotly.getImage({ 'data': data }, layout, (err, imageData) => {
-                    if (err) return console.log(err);
-                    var fileStream = fs.createWriteStream('./media/usernumber-graph.png');
-                    imageData.pipe(fileStream);
-                });
-            } catch (error) {
-                console.log(error);
-                message.channel.send('There was an error in the graphing process');
-            }
+            await generatePlot(rows);
             let diffs = [];
             for (let i = 0; i < rows.length; i++) {
                 if (i != rows.length - 1) {
@@ -50,7 +59,9 @@ module.exports = {
             if (avgUserChange <= 0) {
                 chngPrefix = '';
             }
+            const fileToAttach = new Discord.MessageAttachment('./media/usernumber-graph.png');
             message.channel.send({
+                files: [fileToAttach],
                 embed: {
                     title: "GreenMesa Stats",
                     fields: [
@@ -70,7 +81,7 @@ module.exports = {
                             inline: true
                         },
                         {
-                            name: "Avg. user change over last 24 hr",
+                            name: "Avg. user change (24hr)",
                             value: chngPrefix + avgUserChange + "/hr"
                         },
                         {
@@ -79,18 +90,19 @@ module.exports = {
                             inline: true
                         },
                         {
-                            name: "Total Commands Executed (- trivia quickstart)",
+                            name: "Total Commands Completed",
                             value: config.commandsExecutedCount,
                             inline: true
                         }
                     ],
+                    image: {
+                        url: "attachment://usernumber-graph.png"
+                    },
                     footer: {
                         text: "Graphing in development"
                     }
                 },
-            })
-                .then(message.channel.send({ files: ["./media/usernumber-graph.png"] }))
-                .catch(console.error)
+            }).catch(console.error)
             
         });//first query
     }
