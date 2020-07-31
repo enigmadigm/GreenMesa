9a76e02d68de50a6f7b567b4ca10a4fe9eb3134b
PM <polyversialmind@gmail.com>
2020-07-31 09:36:48 -0500

    trying new db manage system, methods, and more

--- commands/xp.js
+++ commands/xp.js
@@ -1,16 +1,18 @@
 // NOTE: This whole xp system is in long-term development and needs work. The updates will probably come with a web console if there ever is one.
+const { getXP } = require("../dbmanager");
+//const xlg = require("../xlogger");
 
 module.exports = {
     name: 'xp',
     description: 'Get the current amount of xp for the person requested or the author of the message. This system is explained elsewhere, but it should be known that xp is earned by sending messages of any kind globally (on any server GreenMesa is in).',
     usage: "[other user]",
-    execute(client, message, args, conn) {
+    async execute(client, message, args) {
         let target = message.mentions.users.first() || ((message.guild && message.guild.available) ? message.guild.members.cache.get(args[1]) : false) || message.author;
-        conn.query(`SELECT * FROM dgmxp WHERE id = '${target.id}'`, (err, rows) => {
-            if (err) throw err;
-            if (!rows[0]) return message.channel.send("This user has no XP on record.");
-            let xp = rows[0].xp;
-            message.channel.send(target.tag + " currently has " + xp + "xp");
-        });
+
+        let rows = await getXP(message, target);
+
+        if (!rows[0]) return message.channel.send("This user has no XP on record.");
+        let xp = rows[0].xp;
+        message.channel.send(target.tag + " currently has " + xp + "xp");
     }
 }
