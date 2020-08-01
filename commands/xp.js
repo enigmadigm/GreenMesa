277b0f4098e07a8acd9b7013b4e07be9a3d93c68
PM <polyversialmind@gmail.com>
2020-08-01 05:43:02 -0500

    better xp cmd look

--- commands/xp.js
+++ commands/xp.js
@@ -1,6 +1,7 @@
 // NOTE: This whole xp system is in long-term development and needs work. The updates will probably come with a web console if there ever is one.
-const { getXP } = require("../dbmanager");
-//const xlg = require("../xlogger");
+const { getGlobalSetting, getXP } = require("../dbmanager");
+const xlg = require("../xlogger");
+//const { getPermLevel } = require('../permissions');
 
 module.exports = {
     name: 'xp',
@@ -9,10 +10,30 @@ module.exports = {
     async execute(client, message, args) {
         let target = message.mentions.users.first() || ((message.guild && message.guild.available) ? message.guild.members.cache.get(args[1]) : false) || message.author;
 
+        let wec_gs = await getGlobalSetting("warn_embed_color");
         let rows = await getXP(message, target);
+        let warn_embed_color = parseInt(wec_gs[0].value);
+        let iec_gs = await getGlobalSetting("info_embed_color");
 
-        if (!rows[0]) return message.channel.send("This user has no XP on record.");
-        let xp = rows[0].xp;
-        message.channel.send(target.tag + " currently has " + xp + "xp");
+        if (!rows[0]) {
+            return message.channel.send({
+                embed: {
+                    "title": "This user has no XP on record.",
+                    "description": "To gain XP send messages in chat.",
+                    "color": warn_embed_color || 16750899,
+                    "timestamp": new Date(),
+                    "footer": {
+                        "text": this.name
+                    }
+                }
+            }).catch(xlg.error);
+        }
+
+        message.channel.send({
+            embed: {
+                description: `${target} currently has ${rows[0].xp} xp`,
+                color: parseInt(iec_gs[0].value) || 0
+            }
+        });
     }
 }
