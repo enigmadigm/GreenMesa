02d5a441408c5d43844768f8637a5e94be37376f
PM <polyversialmind@gmail.com>
2020-08-22 02:25:55 -0500

    voting improvements now sending a vote with content creates a colored embed and deletes msg

--- commands/vote.js
+++ commands/vote.js
@@ -1,12 +1,34 @@
+const xlg = require("../xlogger");
+const { getGlobalSetting } = require("../dbmanager");
+
 module.exports = {
     name: 'vote',
     description: 'Call a quick vote on the message the command was in',
     usage: "[the content of the vote]",
     guildOnly: true,
-    async execute(client, message) {
-        await message.react('✅')
+    async execute(client, message, args) {
+        if (!args.length) {
+            await message.react('✅')
+                .catch(console.error);
+            return message.react('❌')
+                .catch(console.error);
+        }
+        message.delete().catch();
+        let iec_gs = await getGlobalSetting("info_embed_color");
+        let info_embed_color = parseInt(iec_gs[0].value);
+        let voteEmbed = await message.channel.send({
+            embed: {
+                color: info_embed_color,
+                title: "Vote",
+                description: args.join(" "),
+                footer: {
+                    text: `by ${message.author.tag}`
+                }
+            }
+        }).catch(xlg.error);
+        await voteEmbed.react('✅')
             .catch(console.error);
-        message.react('❌')
+        voteEmbed.react('❌')
             .catch(console.error);
 
     }
