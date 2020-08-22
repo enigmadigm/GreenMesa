d17a4684ab486e1003345d6cc16b90a351960539
PM <polyversialmind@gmail.com>
2020-08-22 01:52:55 -0500

    say perms change must be mod to use `say`

--- commands/say.js
+++ commands/say.js
@@ -1,9 +1,12 @@
+const { permLevels } = require("../permissions");
+
 module.exports = {
     name: 'say',
-    description: 'Make the bot say something. *currently public, may change*',
+    description: 'Make the bot say something. Must be moderator.',
     args: true,
     usage: "<bot message>",
     guildOnly: true,
+    permLevel: permLevels.mod,
     execute(client, message, args) {
         message.delete().catch();
         message.channel.send(args.join(" "));
