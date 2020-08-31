f22530d1282a7adff57e333f0c800f0f866e191e
PM <polyversialmind@gmail.com>
2020-08-31 15:40:21 -0500

    purge improvs Now you can target specific users, and it is better at handling messages

--- commands/purge.js
+++ commands/purge.js
@@ -4,21 +4,45 @@ module.exports = {
     name: 'purge',
     description: 'This command removes all messages from all users in the channel, up to 100',
     aliases: ['bulkdelete'],
-    usage: "<# of messages to delete>",
+    usage: "<# of messages to delete> [target member @]",
     args: true,
     guildOnly: true,
     permLevel: permLevels.mod,
     async execute(client, message, args) {
-        // get the delete count, as an actual number.
-        const deleteCount = parseInt(args[0], 10);
+
+        const targetMember = message.mentions.users.first();
+        const deleteCount = parseInt(args[0], 10); // get the delete count, as an actual number.
 
         // Ooooh nice, combined conditions. <3
-        if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-            return message.reply("please provide a number between 2 and 100 for the number of messages to delete");
-
+        if (!deleteCount || deleteCount < 2)
+            return message.reply("provide a number (2-100) for the number of messages to delete");
+        
+        let opts = {
+            limit: (deleteCount < 100) ? deleteCount : 100,
+            before: message.id
+        }
+        if (targetMember) {
+            opts = {
+                limit: 100,
+                before: targetMember.lastMessageID
+            }
+        }
+        
         // So we get our messages, and delete them. Simple enough, right?
-        const fetched = await message.channel.messages.fetch({ limit: deleteCount });
-        message.channel.bulkDelete(fetched)
-            .catch(error => message.reply(`couldn't delete messages because of: ${error}`));
+        message.channel.messages.fetch( opts ).then(async messages => {
+            if (targetMember) {
+                messages = messages.filter(m => m.author.id === targetMember.id).array().slice(0, deleteCount);
+            }
+            if (messages.length == 0) {
+                return message.channel.send(`Could not find any recent messages.`);
+            }
+
+            message.channel.bulkDelete(messages)
+                .then(message.delete())
+                .catch(e => {
+                    console.log(e.stack);
+                    message.reply(`couldn't delete messages because of: ${e}`);
+                });
+        });
     }
 }
