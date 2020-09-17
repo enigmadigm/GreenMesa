c271a21377cd4c98a2155dcd8b48e1b330862e59
PM <polyversialmind@gmail.com>
2020-09-17 18:22:17 -0500

    new support command It returns the Comaruse server invite.

---
+++ commands/support.js
@@ -0,0 +1,16 @@
+const { getGlobalSetting } = require('../dbmanager')
+const xlg = require('../xlogger');
+
+module.exports = {
+    name: 'support',
+    description: 'get invite to (new) support server',
+    async execute(client, message) {
+        let info_embed_color = await getGlobalSetting('info_embed_color').then(r => parseInt(r[0].value));
+        message.channel.send({
+            embed: {
+                color: info_embed_color,
+                description: `Sure thing, here's the invite to the (new) support server: [invite link](https://discord.gg/AvXvvSg)`
+            }
+        }).catch(xlg.error);
+    }
+}
