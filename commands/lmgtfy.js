7f543590bd454ad7a14bfa858f4c4d1a7e5171a6
PM <polyversialmind@gmail.com>
2020-09-23 08:30:56 -0500

    major lmgtfy changes (google version preview)

--- commands/lmgtfy.js
+++ commands/lmgtfy.js
@@ -1,3 +1,6 @@
+const puppeteer = require('puppeteer');
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'lmgtfy',
     description: 'teach an idiot how to google, or just get a search link',
@@ -6,10 +9,11 @@ module.exports = {
     args: true,
     guildOnly: false,
     category: 'fun',
-    execute(client, message, args) {
+    async execute(client, message, args) {
         let sengine = "google.com/search";
         let iie = "";
         let plainText = false;
+        let sc;
         if (args.join(' ').startsWith('-e -t') || args.join(' ').startsWith('-t -e')) {
             sengine = "lmgtfy.com/";
             iie = "&iie=1";
@@ -25,11 +29,30 @@ module.exports = {
             args.shift();
         } else {
             plainText = false;
+            message.channel.startTyping();
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(`https://google.com/search?q=${args.join("+")}`);
+            sc = await page.screenshot();
+            await browser.close();
         }
         let sterms = args.join("+");
         if (plainText == true) {
             message.channel.send(`https://${sengine}?q=${sterms}${iie}`).catch(console.error);
         } else {
+            if (sc) {
+                const embed = {
+                    "description": `[Let Me Get That For You](https://${sengine}?q=${sterms}${iie})`,
+                    "color": 15277667,
+                    "image": {
+                        "url": 'attachment://screenshot.png'
+                    }
+                }
+                const scfile = new Discord.MessageAttachment(sc, 'screenshot.png');
+                await message.channel.send({ files: [scfile], embed: embed }).catch(console.error);
+                message.channel.stopTyping();
+                return;
+            }
             message.channel.send({
                 embed: {
                     "description": `[Your answer](https://${sengine}?q=${sterms}${iie})`,
