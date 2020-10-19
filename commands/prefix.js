a674e8c5f8b0509546df982afd6b5e653586d450
PM <polyversialmind@gmail.com>
2020-10-19 04:01:10 -0500

    improving look of prefix

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,16 +1,18 @@
 const xlg = require("../xlogger");
 const { permLevels, getPermLevel } = require("../permissions");
 const { setPrefix } = require('../dbmanager')
+const { getGlobalSetting } = require("../dbmanager")
 
 module.exports = {
     name: "prefix",
-    description: "set or view the prefix for your guild",
+    description: "set or view the prefix for guild",
     guildOnly: true,
     category: 'utility',
     async execute(client, message, args) {
         if (!args.length) {
             return message.channel.send({
                 embed: {
+                    color: parseInt((await getGlobalSetting('info_embed_color'))[0].value),
                     title: `${message.gprefix}`,
                     description: `guild prefix`
                 }
@@ -18,10 +20,16 @@ module.exports = {
         }
         var permLevel = await getPermLevel(message.member);
         if (args.length > 0) {
-            if (permLevel < permLevels.admin) return message.channel.send("Insufficient permissions.").catch(xlg.error);
+            if (permLevel < permLevels.admin) return message.channel.send(":frowning2: Insufficient permissions.").catch(xlg.error);
             if (args.join(" ").length > 46) return message.channel.send(":frowning2: Prefix must be less than 47 characters.");
             await setPrefix(message.guild.id, args.join(" "));
-            return message.channel.send("Prefix updated.");
+            message.channel.send({
+                embed: {
+                    color: parseInt((await getGlobalSetting('success_embed_color'))[0].value),
+                    description: `prefix updated`
+                }
+            }).catch(xlg.error);
+            return;
         }
     }
 }
