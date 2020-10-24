c9baa0153b55101e5d53413c63013dd6f0ef8bff
PM <polyversialmind@gmail.com>
2020-10-24 02:04:03 -0500

    cmm error handling and stuff

--- commands/changemymind.js
+++ commands/changemymind.js
@@ -12,31 +12,47 @@ module.exports = {
     usage: '<text>',
     args: true,
     async execute(client, message, args) {
-        if (args.length > 1000) return message.channel.send({
-            embed: {
-                color: parseInt((await getGlobalSetting("fail_embed_color"))[0].value, 10),
-                title: "Nope",
-                description: "I will not accept text longer than 1000 characters."
+        try {
+            message.channel.startTyping();
+            if (args.length > 1000) {
+                message.channel.send({
+                    embed: {
+                        color: parseInt((await getGlobalSetting("fail_embed_color"))[0].value, 10),
+                        title: "Nope",
+                        description: "I will not accept text longer than 1000 characters."
+                    }
+                });
+                message.channel.stopTyping();
+                return false;
             }
-        })
-        var url = `https://nekobot.xyz/api/imagegen?type=changemymind&text=${args.join(" ")}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(async j => {
-                if (j.status == 200 && j.success && j.message && validURL(j.message)) {
-                    message.channel.send({
-                        embed: {
-                            color: parseInt((await getGlobalSetting("info_embed_color"))[0].value, 10),
-                            image: {
-                                url: j.message
-                            },
-                            footer: {
-                                text: "changemymind | neko"
+            var url = `https://nekobot.xyz/api/imagegen?type=changemymind&text=${args.join(" ")}`;
+            await fetch(url)
+                .then(res => res.json())
+                .then(async j => {
+                    if (j.status == 200 && j.success && j.message && validURL(j.message)) {
+                        message.channel.send({
+                            embed: {
+                                color: parseInt((await getGlobalSetting("info_embed_color"))[0].value, 10),
+                                image: {
+                                    url: j.message
+                                },
+                                footer: {
+                                    text: "changemymind | neko"
+                                }
                             }
-                        }
-                    }).catch(xlg.log);
+                        }).catch(xlg.log);
+                    }
+                })
+            message.channel.stopTyping();
+        } catch (error) {
+            xlg.error(error);
+            message.channel.stopTyping().catch(xlg.error);
+            message.channel.send({
+                embed: {
+                    color: parseInt((await getGlobalSetting('fail_embed_color'))[0].value),
+                    description: `Failure while generating image`
                 }
-            })
-            .catch(xlg.log);
+            }).catch(xlg.error);
+        }
     }
 }
