e7c021ff6d83cda44e4fa3446109bbd4812f680f
PM <polyversialmind@gmail.com>
2020-11-26 01:12:11 -0600

    added a confirmation dialogue and some stuff

--- commands/addtwitch.js
+++ commands/addtwitch.js
@@ -3,6 +3,7 @@ const { permLevels } = require('../permissions');
 const { stringToChannel } = require("../utils/parsers");
 const { addTwitchWebhook } = require("../website/routes/twitch");
 const { getGlobalSetting } = require("../dbmanager");
+const Discord = require("discord.js");
 
 module.exports = {
     name: "addtwitch",
@@ -56,15 +57,76 @@ module.exports = {
                 client.specials.sendError(message.channel, "A response message was not received within the time limit, the setup wizard has been cancelled.");
                 return false;
             } else {
-                notifmsg = msgCollected.first().content;
+                if (msgCollected.first().content.toLowerCase() !== "no") {
+                    notifmsg = msgCollected.first().content;
+                }
             }
 
+            
+
+            let confMsg = await message.channel.send({
+                content: message.author,
+                embed: {
+                    color: parseInt(process.env.NAVY_COLOR),
+                    title: "Confirm",
+                    description: `Please confirm that you would like to proceed and complete the setup`
+                }
+            }).catch(xlg.error);
+            await confMsg.react("🟢").catch(xlg.error);
+
+            const filter = (r, u) => r.emoji.name === '🟢' && (message.guild.members.cache.get(u.id).permissions.has(["ADMINISTRATOR"]) || u.id === message.author.id);
+            const collected = await confMsg.awaitReactions(filter, { max: 1, time: 60000 });
+            if (!collected || !collected.size) {
+                confMsg.embeds[0].color = parseInt((await getGlobalSetting("fail_embed_color"))[0].value, 10);
+                confMsg.embeds[0].title = null;
+                confMsg.embeds[0].description = "Aborted setup";
+                await confMsg.edit(new Discord.MessageEmbed(confMsg.embeds[0])).catch(xlg.error);
+
+                const reactsToRemove = confMsg.reactions.cache.filter(r => r.users.cache.has(client.user.id));
+                try {
+                    for (const reaction of reactsToRemove.values()) {
+                        await reaction.users.remove(client.id);
+                    }
+                } catch (error) {
+                    xlg.error(error);
+                }
+            } else {
+                confMsg.embeds[0].color = parseInt((await getGlobalSetting("info_embed_color"))[0].value, 10)
+                confMsg.embeds[0].title = null;
+                confMsg.embeds[0].description = `Proceeding`;
+                await confMsg.edit(new Discord.MessageEmbed(confMsg.embeds[0]));
+
+                const reactsToRemove = confMsg.reactions.cache.filter(r => r.users.cache.has(client.user.id));
+                try {
+                    for (const reaction of reactsToRemove.values()) {
+                        await reaction.users.remove(client.id);
+                    }
+                } catch (error) {
+                    xlg.error(error);
+                }
+            }
 
             const hookRes = await addTwitchWebhook(targetUsername, false, message.guild.id, targetChannel, notifmsg);
             if (!hookRes) {
-                client.specials.sendError(message.channel, "Error When Creating Subscription With Twitch");
+                client.specials.sendError(message.channel, "Error when creating subscription with Twitch");
                 return false;
+            } else if (hookRes === "ID_NOT_FOUND") {
+                client.specials.sendError(message.channel, `Your streamer, \`${targetUsername}\`, could not be found`);
+                return false;
+            } else if (hookRes === "ALREADY_EXISTS") {
+                client.specials.sendError(message.channel, `A subscription for \`${targetUsername}\` already exists`);
+                return false;
+            } else {
+                await message.channel.send({
+                    embed: {
+                        color: parseInt((await getGlobalSetting("success_embed_color"))[0].value, 10),
+                        title: "Subscribed",
+                        description: `You server is now subscribed to notifications in ${targetChannel} for when **${targetUsername}** goes live.`
+                    }
+                });
+                return;
             }
+
         } catch (error) {
             xlg.error(error);
             await client.specials.sendError(message.channel);
