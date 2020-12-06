adeeedf4448b9b7390df5e6269dce5621f78fec9
PM <polyversialmind@gmail.com>
2020-12-06 01:30:47 -0600

    updated embed color fetch

--- commands/support.js
+++ commands/support.js
@@ -1,11 +1,11 @@
-const { getGlobalSetting } = require('../dbmanager')
 const xlg = require('../xlogger');
+const { getGlobalSetting } = require('../dbmanager')
 
 module.exports = {
     name: 'support',
     description: 'get invite to (new) support server',
     async execute(client, message) {
-        let info_embed_color = await getGlobalSetting('info_embed_color').then(r => parseInt(r[0].value));
+        let info_embed_color = parseInt((await getGlobalSetting("info_embed_color"))[0].value, 10);
         message.channel.send({
             embed: {
                 color: info_embed_color,
