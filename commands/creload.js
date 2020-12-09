42d44deb0cd85f287b1eba95a16d5e0f050fe5de
PM <polyversialmind@gmail.com>
2020-12-09 13:39:11 -0600

    rid of ping in creload

--- commands/creload.js
+++ commands/creload.js
@@ -12,7 +12,7 @@ module.exports = {
         const command = message.client.commands.get(commandName)
             || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-        if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+        if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\``);
 
         // Deleting old command from cache
         delete require.cache[require.resolve(`./${command.name}.js`)];
