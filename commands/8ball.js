7423ef2b4e8fb81556228c6db1512bad628b59fe
PM <polyversialmind@gmail.com>
2020-12-28 12:05:34 -0600

    8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,8 +1,10 @@
+const { args } = require("./inrole");
+
 /**
  * Generates the magic 8ball response
  */
 function doMagic8BallVoodoo() {
-    var rand = ['Yes', 'No', 'idk, maybe', 'occasionally', 'in your dreams', "when you're dead that will be true"];
+    var rand = ['Yes', 'No', 'idk, maybe', 'occasionally', 'in your dreams', "when you're dead that will be true", "never", "that could happen if you sacrifice something you love"];
     return rand[Math.floor(Math.random() * rand.length)];
 }
 
@@ -10,7 +12,11 @@ module.exports = {
     name: '8ball',
     description: 'play some *magic* 8ball (not pool)',
     category: 'fun',
+    args: true,
     execute(client, message) {
+        if (args.join(" ").toLowerCase() === "what is the meaning of the universe" || args.join(" ").toLowerCase() === "what is the meaning of the universe?") {
+            message.channel.send("42").catch(console.error);
+        }
         message.channel.send(doMagic8BallVoodoo()).catch(console.error);
     }
 }
