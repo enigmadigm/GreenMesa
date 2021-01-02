6127da795a3f515c4e526a98a2881295fcea0cf5
PM <polyversialmind@gmail.com>
2021-01-02 00:18:15 -0600

    new isgd/vgd commands

--- commands/isgd.js
+++ commands/isgd.js
@@ -4,11 +4,11 @@ const { getGlobalSetting } = require("../dbmanager");
 const fetch = require("node-fetch");
 
 module.exports = {
-    name: "tinyurl",
+    name: "isgd",
     //aliases: [""],
     description: {
-        short: "shorten a link with tinyurl",
-        long: "This command will take any valid link and shorten it to the domain tinyurl.com."
+        short: "shorten a link with is.gd",
+        long: "This command will take any valid link and shorten it to the public domain [is.gd](https://is.gd). URLs should not contain whitespace, and may be limited in length."
     },
     category: "utility",
     usage: "<url>",
@@ -16,7 +16,6 @@ module.exports = {
     specialArgs: 1,
     permLevel: permLevels.member,
     guildOnly: false,
-    ownerOnly: false,
     async execute(client, message, args) {
         try {
             if (args.length > 1) {
@@ -24,12 +23,21 @@ module.exports = {
                 return;
             }
             const url = args[0].slice(0, 1024);
-            const r = await fetch(`http://tinyurl.com/api-create.php?url=${url}`);
+            const r = await fetch(`https://is.gd/create.php?format=json&url=${args[0]}`);
             if (r.status !== 200) {
-                await client.specials.sendError(message.channel, "URL not shortened", true);
+                await client.specials.sendError(message.channel, "Received a non-ok response code from is.gd", true);
                 return;
             }
-            const j = await r.text();
+            const j = await r.json();
+            if (!j || j.errorcode || !j.shorturl) {
+                if (j.errorcode === 1 && j.errormessage) {
+                    await client.specials.sendError(message.channel, `Could not shorten URL:\n${j.errormessage}`, true);
+                    return;
+                } else {
+                    await client.specials.sendError(message.channel, `Could not shorten URL`, true);
+                    return;
+                }
+            }
             message.channel.send({
                 embed: {
                     color: parseInt((await getGlobalSetting("info_embed_color"))[0].value, 10),
@@ -41,7 +49,7 @@ module.exports = {
                         },
                         {
                             name: "Output",
-                            value: `${j}`
+                            value: `[${j.shorturl.replace("https://", "")}](${j.shorturl})`
                         }
                     ],
                     footer: {
