f580a88f7bfe9c150d33f28689567feeaead31d6
PM <polyversialmind@gmail.com>
2021-01-23 18:13:33 -0600

    attempting again to change headers

--- website/app.js
+++ website/app.js
@@ -21,8 +21,8 @@ class MesaWebsite {
         this.client = client;
         this.app = express();
         
-        this.app.use(helmet());// will have to see if this actually protects
-        this.app.disable('x-powered-by');
+        this.app.use(helmet({ frameguard: { action: "deny" } }));// will have to see if this actually protects
+        //this.app.set('x-powered-by', false);
         this.app.set('etag', false);
         //this.app.use(express.json());// THIS IS WHAT WAS BREAKING THE TWITCH SECURITY MIDDLEWARE
         this.app.use(express.urlencoded({ extended: false }));
@@ -37,6 +37,10 @@ class MesaWebsite {
         }));
         this.app.use(passport.initialize());
         this.app.use(passport.session());
+        this.app.use(function (req, res, next) {
+            res.header("x-powered-by", "Sadness")
+            next();
+        });
         this.app.use(express.static(path.join(__dirname, STATIC), {
             index: false,
             extensions: ['html']
