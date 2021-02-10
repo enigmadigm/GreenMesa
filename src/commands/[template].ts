272b04658f9bfa514b492111fad341cd8016cfcb
Rooskie <polyversialmind@gmail.com>
2021-02-09 22:47:07 -0600

    template update

--- src/commands/[template].ts
+++ src/commands/[template].ts
@@ -1,20 +1,6 @@
 import xlg from "../xlogger";
 import { permLevels } from '../permissions';
 import { Command } from "src/gm";
-//const { getGlobalSetting } = require("../dbmanager");
-
-/*export class Template implements Command {
-    name = "";
-    async execute(client: XClient, message: XMessage, args: string[]): Promise<boolean | void> {
-        try {
-            //
-        } catch (error) {
-            xlg.error(error);
-            await client.specials.sendError(message.channel);
-            return false;
-        }
-    }
-}*/
 
 export const command: Command = {
     name: "",
@@ -42,5 +28,3 @@ export const command: Command = {
         }
     }
 }
-
-
