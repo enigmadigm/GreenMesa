fae4cf6cc156976255224b0b90e1de8427598d6a
Rooskie <polyversialmind@gmail.com>
2021-02-26 01:05:46 -0600

    xkcd command

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -277,3 +277,7 @@ export function durationToString(duration: number): string {
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function randomIntFromInterval(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
