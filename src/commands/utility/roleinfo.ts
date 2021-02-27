1f1607a0ca7181e55fd29460eda577b9850f1704
Rooskie <polyversialmind@gmail.com>
2021-02-26 23:56:38 -0600

    roleinfo command + formatting

---
+++ src/commands/utility/roleinfo.ts
@@ -0,0 +1,46 @@
+import xlg from "../../xlogger";
+import { permLevels } from '../../permissions';
+import { Command } from "src/gm";
+import { stringToRole } from "../../utils/parsers";
+
+export const command: Command = {
+    name: "roleinfo",
+    aliases: ["ri"],
+    description: {
+        short: "get information on a role",
+        long: "Get information on a role."
+    },
+    usage: "<role>",
+    args: true,
+    cooldown: 1,
+    permLevel: permLevels.trustedMember,
+    guildOnly: true,
+    async execute(client, message, args) {
+        try {
+            if (!message.guild) return;
+
+            const role = stringToRole(message.guild, args.join(" "), true, true, false);
+            if (!role || role === "@everyone" || role === "@here") {
+                client.specials?.sendError(message.channel, "A valid role could not be found");
+                return;
+            }
+            await message.channel.send({
+                embed: {
+                    color: role.color > 150 ? role.color : await client.database?.getColor("info_embed_color"), 
+                    description:
+`Role info for ${role}
+
+ID: ${role.id}
+Mentionable: ${role.mentionable}
+Permissions Int: ${role.permissions.bitfield}
+Color Hex: ${role.hexColor}
+`
+                }
+            })
+        } catch (error) {
+            xlg.error(error);
+            await client.specials?.sendError(message.channel);
+            return false;
+        }
+    }
+}
