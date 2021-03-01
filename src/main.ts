87bcd9150f5e1c12d49af4507b0e00d12a68525f
Rooskie <polyversialmind@gmail.com>
2021-03-01 01:22:21 -0600

    trying for auto shard respawn

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { ShardingManager } from 'discord.js';
 import config from "../auth.json";
 import xlg from './xlogger';
+
 const manager = new ShardingManager('./dist/bot.js', { token: config.token });
 
 manager.on('shardCreate', shard => {
     xlg.log(`Launched shard ${shard.id}`);
+    shard.on("death", () => {
+        shard.respawn();
+    });
 });
 manager.spawn();
