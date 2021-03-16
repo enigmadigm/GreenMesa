00c2f9883e200d222e0ffb931f2396f2dccf28bf
Rooskie <polyversialmind@gmail.com>
2021-03-16 01:49:48 -0500

    rmnote command

--- src/commands/moderation/rmnote.ts
+++ src/commands/moderation/rmnote.ts
@@ -2,17 +2,16 @@ import xlg from "../../xlogger";
 import { permLevels } from '../../permissions';
 import { Command, UserNote } from "src/gm";
 import { stringToMember } from "../../utils/parsers";
-import moment from 'moment';
 
 export const command: Command = {
-    name: "editnote",
+    name: "rmnote",
     description: {
-        short: "edit user notes",
-        long: "Edit a member's note. Specify the note id and the new content."
+        short: "delete user notes",
+        long: "Delete a member's note."
     },
-    usage: "<member> <id> <new note>",
+    usage: "<member> <id>",
     examples: [
-        "sm editnote @Darth 6 Pissed off the mods again today"
+        "sm rmnote @Darth 6"
     ],
     args: true,
     cooldown: 2,
@@ -29,7 +28,7 @@ export const command: Command = {
             }
             args.shift();
             if (!args.length) {
-                await client.specials?.sendError(message.channel, `You did not specify an id\n\`<user> <id> <note>\``);
+                await client.specials?.sendError(message.channel, `You did not specify an id\n\`<user> <id>\``);
                 return;
             }
             if (!/^[0-9]{1,100}$/.test(args[0])) {
@@ -38,32 +37,22 @@ export const command: Command = {
             }
             const id = parseInt(args[0], 10);
             args.shift();
-            if (!args.length) {
-                await client.specials?.sendError(message.channel, `You did not specify a note\n\`<user> <id> <note>\``);
-                return;
-            }
-            const a = args.join(" ");
             const gud = await client.database?.getGuildUserData(message.guild.id, target.id);
             if (!gud) {
                 await client.specials?.sendError(message.channel, `Could not retrieve user info`);
                 return;
             }
             const notes: UserNote[] = gud.modnote && gud.modnote.length ? JSON.parse(gud.modnote) : [];
-            if (a.length > 500) {
-                client.specials?.sendError(message.channel, `You have exceeded the **500** character limit. You are over by **${a.length - 500}** characters.`)
-                return;
-            }
             const note = notes.find((x) => x.id === id);
             if (!note) {
                 await client.specials?.sendError(message.channel, `A note with that ID does not exist`);
                 return;
             }
-            note.content = a;
-            note.updated = moment().format('YYYY-MM-DD HH:mm:ss');
+            notes.splice(notes.indexOf(note), 1);
 
             gud.modnote = JSON.stringify(notes).escapeSpecialChars();
             await client.database?.updateGuildUserData(gud);
-            await client.specials?.sendInfo(message.channel, "Note edited");
+            await client.specials?.sendInfo(message.channel, "Note deleted");
         } catch (error) {
             xlg.error(error);
             await client.specials?.sendError(message.channel);
