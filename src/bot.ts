cd23de74ed1987d8158f30675776fe7eddda2fd9
Rooskie <polyversialmind@gmail.com>
2021-03-30 01:04:37 -0500

    fix(regex): string escape bug fix

--- src/bot.ts
+++ src/bot.ts
@@ -25,14 +25,14 @@ process.on('unhandledRejection', async (reason, promise) => {
 });
 
 String.prototype.escapeSpecialChars = function () {
-    return this.replace(/\\n/g, "\\n")
-        .replace(/'/g, "\\'")
-        .replace(/"/g, '\\"')
-        .replace(/&/g, "\\&")
-        .replace(/r/g, "\\r")
-        .replace(/t/g, "\\t")
-        .replace(/b/g, "\\b")
-        .replace(/f/g, "\\f");
+    return this.replace(/\\n/g, "\\\\n")
+        .replace(/\\'/g, "\\\\'")
+        .replace(/\\"/g, '\\\\"')
+        .replace(/\\&/g, "\\\\&")
+        .replace(/\\r/g, "\\\\r")
+        .replace(/\\t/g, "\\\\t")
+        .replace(/\\b/g, "\\\\b")
+        .replace(/\\f/g, "\\\\f");
 };
 
 String.prototype.escapeDiscord = function () {
