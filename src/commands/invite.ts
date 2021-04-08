51c13598519dd656b9f10825f7796ec253ce5505
Rooskie <polyversialmind@gmail.com>
2021-04-07 23:12:02 -0500

    fix(invite): invite formatting and link functions

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -6,13 +6,13 @@ export const command: Command = {
     usage: "[id of a server you can invite on]",
     async execute(client, message, args) {
         let guildIdParam = "";
-        if (args.length && args.length == 1 && !isNaN(parseInt(args[0])) && args[0].toString().length == 18) {
-            guildIdParam = `&guild_id=${args[0]}`;
+        if (args.length && args.length == 1 && /^[0-9]{18}$/.test(args.join(" "))) {
+            guildIdParam = `${args[0]}`;
         }
         message.channel.send({
             embed: {
                 color: await client.database.getColor("info"),
-                description: `Add ${client.user?.username} to your server **[here](https://stratum.hauge.rocks/invite${guildIdParam ? `/${guildIdParam}` : ""})**.\n\n**IMPORTANT:** Initial issues are likely caused by misconfigured permissions.\n\n[Steps to get bot (low effort)](https://git.io/fjmEX).`,
+                description: `Add ${client.user?.username} to your server **[here](${client.specials.getBackendRoot()}/invite${guildIdParam ? `/${guildIdParam}` : ""})**.\n\n**IMPORTANT:** Initial issues are likely caused by misconfigured permissions.\n\n[Steps to get bot (low effort)](https://git.io/fjmEX).`,
                 timestamp: new Date().getTime(),
                 footer: {
                     icon_url: client.user?.avatarURL() || undefined,
