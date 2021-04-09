0580582481dffef61ed1061612d3190a5a016af0
Rooskie <polyversialmind@gmail.com>
2021-04-09 18:35:26 -0500

    fix(dashboard): embark redirects and messages

--- src/website/client/src/pages/Embark/index.tsx
+++ src/website/client/src/pages/Embark/index.tsx
@@ -10,21 +10,55 @@ function useQuery() {
 export function Embark() {
     const query = useQuery();
     const id = query.get("guild_id");
+    const [tron, setTron] = React.useState("");
 
     React.useEffect(() => {
+        if (window.opener && window.parent && id) {
+            setTron("Closing to dashboard")
+        } else if (window.opener && window.parent) {
+            setTron("Closing")
+        } else if (id) {
+            setTron("Redirecting to dashboard")
+        } else {
+            setTron("Enjoy infinitely loading")
+        }
         setTimeout(() => {
-            window.onunload = () => {
-                if (id) {
-                    window.opener.location.pathname = `/dash/${id}`;
+            if (window.opener) {
+                setTron("Goodbye")
+                window.onunload = () => {
+                    if (id) {
+                        window.opener.location.pathname = `/dash/${id}`;
+                    }
                 }
+                window.close();
+            } else if (id) {
+                setTron("Goodbye")
+                window.location.pathname = `/dash/${id}`;
+            } else {
+                setTron("")
+                // window.location.pathname = `/dash`;
             }
-            window.close();
         }, 2000)
     }, [id])
 
+    const altClick = () => {
+        if (window.opener) {
+            window.close();
+        } else {
+            window.location.pathname = "/dash";
+        }
+    }
+
     return (
-        <div className="embark-wrapper">
-            <PacmanLoader color="#8bc5cd" />
+        <div className="embark-container">
+            <div className="embark-wrapper">
+                <PacmanLoader color="#8bc5cd" />
+            </div>
+            <div className="embark-tron">
+                {tron ? (
+                    <div><button onClick={altClick}>{tron}</button></div>
+                ) : null}
+            </div>
         </div>
     )
 }
