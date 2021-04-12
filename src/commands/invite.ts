ca45644ca4cf315c54add677c11432c90250fd1c
Rooskie <polyversialmind@gmail.com>
2021-04-11 22:42:55 -0500

    feat: dashboard command

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,26 +1,35 @@
+import xlg from '../xlogger';
 import { Command } from "src/gm";
 
 export const command: Command = {
     name: 'invite',
-    description: 'invite link to bring the bot to your server',
+    description: {
+        short: 'get bot in your server',
+        long: 'Invite link to bring the bot to your server.'
+    },
     usage: "[id of a server you can invite on]",
     async execute(client, message, args) {
-        let guildIdParam = "";
-        if (args.length && args.length == 1 && /^[0-9]{18}$/.test(args.join(" "))) {
-            guildIdParam = `${args[0]}`;
-        }
-        message.channel.send({
-            embed: {
-                color: await client.database.getColor("info"),
-                description: `Add ${client.user?.username} to your server **[here](${client.specials.getBackendRoot()}/invite${guildIdParam ? `/${guildIdParam}` : ""})**.\n\n**IMPORTANT:** Initial issues are likely caused by misconfigured permissions.\n\n[Steps to get bot (low effort)](https://git.io/fjmEX).`,
-                timestamp: new Date().getTime(),
-                footer: {
-                    icon_url: client.user?.avatarURL() || undefined,
-                    text: "Invite"
-                }
+        try {
+            let guildIdParam = "";
+            if (args.length && args.length == 1 && /^[0-9]{18}$/.test(args.join(" "))) {
+                guildIdParam = `${args[0]}`;
             }
-        });
-
+            message.channel.send({
+                embed: {
+                    color: await client.database.getColor("info"),
+                    description: `Add ${client.user?.username} to your server **[here](${client.specials.getBackendRoot()}/invite${guildIdParam ? `/${guildIdParam}` : ""})**.\n\n**IMPORTANT:** Initial issues are likely caused by misconfigured permissions.\n\n[Steps to get bot (low effort)](https://git.io/fjmEX).`,
+                    timestamp: new Date().getTime(),
+                    footer: {
+                        icon_url: client.user?.avatarURL() || undefined,
+                        text: "Invite"
+                    }
+                }
+            });
+        } catch (error) {
+            xlg.error(error);
+            await client.specials?.sendError(message.channel);
+            return false;
+        }
     }
 }
 
