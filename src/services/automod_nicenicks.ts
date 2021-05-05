97eb6ae19cf3982b8a7dc057ae4c41660d924e4c
Rooskie <polyversialmind@gmail.com>
2021-05-04 22:50:03 -0500

    fix(nicenicks): cleanup optional message

--- src/services/automod_nicenicks.ts
+++ src/services/automod_nicenicks.ts
@@ -5,7 +5,7 @@ import { GuildMember } from "discord.js";
 
 export const service: MessageService = {
     async getInformation() {
-        return "This will check the nicknames of new members and members who change their nicknames for special characters in patterns that could make the nickname difficult to mention. Basically, if there is are any non-standard english characters (alphabetic characters not found on a QWERTY keyboard) in the nickname, it will be changed to a placeholder nickname.";
+        return "This will check the nicknames of new members and members who change their nicknames for cancerous characters. Basically, if there is are any non-standard english characters (alphabetic characters not found on a QWERTY keyboard) in the nickname, it will be changed to a placeholder nickname.";
     },
     async execute(client, member: GuildMember) {
         try {
@@ -21,9 +21,7 @@ export const service: MessageService = {
                 await member.setNickname("change name (stratum automod)", `automod:nicenicks saw a nondesirable nickname`);
 
                 if (modResult.sendDM) {
-                    await member.user.send(`Hello ${member.user}!
-**Regarding server:** ${member.guild.name}
-My \`nicenicks\` automodule noticed that your nickname isn't very nice to type out. The admins of ${member.guild.name} have requested that I change all user's undesirable nicknames to a placeholder until they change it to something nicer; please do so!`);
+                    await member.user.send(`Automod noticed that your nickname isn't very nice to type out. The admins of ${member.guild.name} have requested that I correct all user's undesirable nicknames to something better until they change it to something nicer; please do so!`);
                     // THIS DM MESSAGE SHOULD BE CONFIGURABLE, IT SHOULD AT LEAST BE SOMETHING THAT CAN BE TOGGLED BY ADMINS ON THE DASHBOARD
                 }
             }
