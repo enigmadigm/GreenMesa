6ca0865312941b5bb22b03e0da072794c7e0d809
Rooskie <polyversialmind@gmail.com>
2021-05-17 22:11:43 -0500

    fix(dashboard): recording sm changes

--- src/website/client/src/components/SuperMessage/index.tsx
+++ src/website/client/src/components/SuperMessage/index.tsx
@@ -12,11 +12,8 @@ interface SuperMessageProps {
 export function SuperMessage(props: SuperMessageProps) {
     const { value, set } = props;
     const [showing, setShowing] = React.useState<boolean>(false);
-    const [editing, setEditing] = React.useState<boolean>(false);
-    const [waiting, setWaiting] = React.useState<boolean>(false);
     const [em, setEm] = React.useState("");
     const [mc, setMc] = React.useState("");
-    const modalWrapper = React.useRef(null);
 
     React.useEffect(() => {}, []);
 
@@ -27,33 +24,7 @@ export function SuperMessage(props: SuperMessageProps) {
         } else {
             setTimeout(() => setMc(""), 100)
         }
-    }, [showing])
-
-    const useOutsideClicker = (ref: React.MutableRefObject<any>) => {// https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
-        React.useEffect(() => {
-            /**
-             * Alert if clicked on outside of element
-             */
-            function handleClickOutside(event: MouseEvent) {
-                if (ref.current && !ref.current.contains(event.target)) {
-                    handleCancelClick();
-                }
-            }
-
-            // Bind the event listener
-            document.addEventListener("mousedown", handleClickOutside);
-            return () => {
-                // Unbind the event listener on clean up
-                document.removeEventListener("mousedown", handleClickOutside);
-            };
-        }, [ref]);
-    }
-
-    useOutsideClicker(modalWrapper);
-
-    const handleOkClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        setShowing(false);
-    };
+    }, [showing]);
 
     const handleCancelClick = () => {
         setShowing(false);
@@ -61,10 +32,9 @@ export function SuperMessage(props: SuperMessageProps) {
         setEm("");
     }
 
-    const handleOpenClick = () => {
+    const handleShowClick = () => {
         handleCancelClick();
         if (!showing) {
-            setEditing(false);
             setShowing(true);
         }
     }
@@ -76,45 +46,57 @@ export function SuperMessage(props: SuperMessageProps) {
     }
 
     const handleFieldAdd = () => {
-        if (!value.embed.fields) {
-            value.embed.fields = [];
+        const m = Object.assign({}, value);
+        if (!m.embed.fields) {
+            m.embed.fields = [];
         }
-        value.embed.fields.push({ name: "", value: "", inline: false });
+        m.embed.fields.push({ name: "", value: "", inline: false });
+        set(m);
     }
-
+    
     const handleFieldDelete = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: number) => {
+        const m = Object.assign({}, value);
+        if (m.embed.fields && m.embed.fields.length) {
+            m.embed.fields.splice(id, 1);
+            set(m);
+        }
     };
 
     return (
         <div>
-            <button onClick={handleOpenClick}>open</button>
+            <button onClick={handleShowClick}>{showing ? "Close Creator" : "Show SuperMessage Creator"}</button>
             {showing ? (
-                <Rootportal show={showing}>
-                    <div className="sm-super">
-                        <div className={`sm-container ${mc}`} ref={modalWrapper}>
-                            <div className="sm-form-container">
-                                {em && (
-                                    <div className="inline-error">
-                                        {em}
-                                    </div>
-                                )}
-                                <p style={{ fontWeight: 700, marginTop: 10, marginBottom: 5 }}>Message</p>
-                                <textarea className="sm-txta"
-                                    rows={7}
-                                    placeholder="Enter message content"
-                                    onChange={handleOutsideChange}
-                                    value={value.outside}
-                                ></textarea>
-                                <br style={{ height: 5 }} />
+                <div className={`sm-super ${mc}`}>
+                    <div className="sm-form-container">
+                        {em && (
+                            <div className="inline-error">
+                                {em}
                             </div>
-                            <div className="sm-buttons">
-                                <hr />
-                                <button className="card-footer-button primary-button" onClick={handleOkClick}>Ok</button>
-                                {/* <button className="card-footer-button twitch-cancel-bottom" onClick={handleCancelClick}>Cancel</button> */}
-                            </div>
-                        </div>
+                        )}
+                        <input type="text" name="smicon" id="" />
+                        <input type="text" name="smauthor" id="" />
+                        <input type="text" name="smauthorurl" id="" />
+                        <input type="text" name="smtitle" id="" />
+                        <input type="text" name="smurl" id="" />
+                        <input type="text" name="smdescription" id="" />
+                        <input type="text" name="" id="" />
+                        <input type="text" name="" id="" />
+                        <input type="text" name="" id="" />
+                        <input type="text" name="" id="" />
+                        <textarea className="sm-txta"
+                            rows={7}
+                            placeholder="Enter message content"
+                            onChange={handleOutsideChange}
+                            value={value.outside}
+                        ></textarea>
+                        <br style={{ height: 5 }} />
                     </div>
-                </Rootportal>
+                    {/* <div className="sm-buttons">
+                        <hr />
+                        <button className="primary-button" onClick={handleOkClick}>Ok</button>
+                    </div> */}
+                </div>
+
             ) : null}
         </div>
     )
