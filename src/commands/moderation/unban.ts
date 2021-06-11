07ab14eeacb22ba6d7ade91e7b7e813751e60dce
Rooskie <polyversialmind@gmail.com>
2021-06-10 21:28:01 -0500

    refactor(command): fix for v13 and streamline kick, ub, um

--- src/commands/moderation/unban.ts
+++ src/commands/moderation/unban.ts
@@ -1,85 +1,92 @@
-
 import { permLevels } from '../../permissions';
-//import { getGuildSetting } from "../dbmanager";
-import { Command } from "src/gm";
+import { Command, GuildMessageProps } from "src/gm";
 import { Contraventions } from "../../utils/contraventions";
+import { isSnowflake } from '../../utils/specials';
 
-export const command: Command = {
+export const command: Command<GuildMessageProps> = {
     name: "unban",
     aliases: ["ub"],
     description: {
-        short: "unban users",
-        long: "Use to unban a banned member or all banned members. This will remove their ban."
+        short: "unban one or all users",
+        long: "Use to unban a banned member or all banned members. This will remove their server ban, if one exists. It will also remove users from the autoban list."
     },
     usage: "<user id | all>",
     args: 1,
     permLevel: permLevels.mod,
     guildOnly: true,
     moderation: true,
+    permissions: ["BAN_MEMBERS", "MANAGE_GUILD"],
     async execute(client, message, args) {
         try {
-            if (!message.guild || !message.member) return;
-
-            if (args.join(" ") === "all") {
-                const b = await message.guild.fetchBans();
-                const bc = b.size;
-                if (!bc) {
-                    await client.specials?.sendError(message.channel, `No bans found`);
+            const a = args.join(" ");
+            await message.guild.bans.fetch();
+            if (a === "all") {
+                let bc = 0;
+                const storedBans = await client.database.getGuildSetting(message.guild, "toban");
+                if (storedBans) {
+                    try {
+                        const bans: string[] = JSON.parse(storedBans.value);
+                        if (bans.length) {
+                            bc += bans.length;
+                            await client.database.editGuildSetting(message.guild, "toban", "[]");
+                        }
+                    } catch (error) {
+                        xlg.error(error);
+                    }
+                }
+                const b = message.guild.bans.cache;
+                if (!bc && !b.size) {
+                    await client.specials.sendError(message.channel, `No bans found`);
                     return;
                 }
                 const ba = b.array();
                 for (let i = 0; i < ba.length; i++) {
                     const c = ba[i];
-                    message.guild.members.unban(c.user);
+                    await message.guild.members.unban(c.user);
+                    bc++;
                 }
-                message.channel.send(`\\✅ ${bc} users unbanned`);
+                await message.channel.send(`\\✅ ${bc} users unbanned`);
                 return;
             }
 
-            if (!/^[0-9]{18}$/.test(args[0])) {
+            const t = args[0];
+            const ub = message.guild.bans.cache.find(x => x.user.id === t || x.user.tag === t);//TODO: add function to specials to search all shards for a user and return the userinfo, like the id, if they are found, this could be used to search using tags because with a tag a user in a mutual guild could be found
+            if (!ub && !isSnowflake(t)) {// if they provided a tag (or something) and the use wasn't found
                 await client.specials?.sendError(message.channel, "A valid user ID (Snowflake) is required");
                 return;
             }
-            const t = args[0];
-            let ub;
-            try {
-                ub = await message.guild.fetchBan(t);//TODO: add function to specials to search all shards for a user and return the userinfo, like the id, if they are found, this could be used to search using tags because with a tag a user in a mutual guild could be found
-            } catch (e) {
-                if (e.message === "Unknown Ban") {
-                    const storedBans = await client.database.getGuildSetting(message.guild, "toban");
-                    if (storedBans) {
-                        try {
-                            const bans: string[] = JSON.parse(storedBans.value);
-                            if (bans.includes(t)) {
-                                bans.splice(bans.indexOf(t), 1);
-                                await client.database.editGuildSetting(message.guild, "toban", JSON.stringify(bans).escapeSpecialChars());
-                                await message.channel.send(`That ID was found and removed from the autoban list.`);
-                                return;
-                            }
-                        } catch (error) {
-                            xlg.error(error);
+            if (!ub) {
+                const storedBans = await client.database.getGuildSetting(message.guild, "toban");
+                if (storedBans) {
+                    try {
+                        const bans: string[] = JSON.parse(storedBans.value);
+                        if (bans.includes(t)) {
+                            bans.splice(bans.indexOf(t), 1);
+                            await client.database.editGuildSetting(message.guild, "toban", JSON.stringify(bans).escapeSpecialChars());
+                            await message.channel.send(`That ID was found and removed from the autoban list.`);
+                            return;
                         }
+                    } catch (error) {
+                        xlg.error(error);
                     }
-
-                    await client.specials?.sendError(message.channel, "There is no banned user matching that ID");
-                    return;
-                } else {
-                    throw new Error("Error fetching ban");
                 }
+    
+                await client.specials.sendError(message.channel, "There is no banned user matching that ID");
+                return;
             }
 
             args.shift();
-            const reason = args.join(" ");
+            const reason = a;
             try {
                 await message.guild.members.unban(ub.user, reason);
                 Contraventions.logUnban(message.guild.id, ub.user.id, message.member, reason, ub.user.tag);
-                message.channel.send(`\\✅ Unbanned ${ub.user.tag}`);
+                await message.channel.send(`\\✅ Unbanned ${ub.user.tag}`).catch(xlg.error);
             } catch (e) {
-                message.channel.send(`\\🆘 Could not unban ${ub.user.tag}`);
+                await message.channel.send(`\\🆘 Could not unban ${ub.user.tag}`);
             }
         } catch (error) {
             xlg.error(error);
-            await client.specials?.sendError(message.channel);
+            await client.specials.sendError(message.channel);
             return false;
         }
     }
