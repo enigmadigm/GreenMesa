20765cc30a590dc4d1a615c40f3adae8281b31d0
Rooskie <polyversialmind@gmail.com>
2021-06-15 01:17:19 -0500

    feat(command): :construction: move xp info to generated image
    this isn't complete, i may need to make my own image generation api, and i need to make sure the data in the image will be accurate
    
    i don't know how reliable this api will be, and there needs to be a fallback if images aren't allowed

--- src/commands/fun/xp.ts
+++ src/commands/fun/xp.ts
@@ -1,4 +1,6 @@
 // NOTE: This whole xp system is in long-term development and needs work. The updates will probably come with a web console if there ever is one.
+import { MessageAttachment, Permissions } from "discord.js";
+import fetch from "node-fetch";
 import { Command, GuildMessageProps } from "src/gm";
 import { stringToMember } from "../../utils/parsers";
 
@@ -20,21 +22,24 @@ export const command: Command<GuildMessageProps> = {
     aliases: ['rank', 'level'],
     usage: "[other user]",
     guildOnly: true,
+    permissions: ["EMBED_LINKS"],
     async execute(client, message, args) {
         try {
             const a = args.join(" ");
-            const target = await stringToMember(message.guild, a, true, true, true);
-            if (!target) {
+            const queriedTarget = await stringToMember(message.guild, a, true, true, true);
+            const target = queriedTarget || message.member;
+            if (a && !queriedTarget) {
                 await message.channel.send(`That's not a real target`);
                 return;
             }
 
             const xp = await client.database.getFullPointsData(target);
+            const personal = await client.database.getXPPersonal(message.guild.id, target.id);
             const xpTypeGlobal = await client.database.getGlobalSetting('xp_type');
             const sym = (xpTypeGlobal) ? xpTypeGlobal.value : 'exp';
 
             if (!xp) {
-                message.channel.send({
+                await message.channel.send({
                     embed: {
                         title: "This user has no XP on record.",
                         description: "To gain XP send messages in chat.",
@@ -44,15 +49,28 @@ export const command: Command<GuildMessageProps> = {
                 return;
             }
 
-            message.channel.send({
-                embed: {
-                    color: await client.database.getColor("info"),
-                    description: `**${target.displayName}** has ${verbs[Math.floor(Math.random() * verbs.length)]} **${sym} ${xp.points}** total at level **${xp.level}**.\n**${sym} ${xp.pointsToGo}** more is needed for level **${xp.level + 1}**.\n${xp.pointsLevelNext - xp.pointsToGo}/${xp.pointsLevelNext} (${Math.round(((xp.pointsLevelNext - xp.pointsToGo) / xp.pointsLevelNext) * 100)}%)`,
-                    footer: {
-                        text: ``
-                    }
+            const percentToNext = Math.round(((xp.pointsLevelNext - xp.pointsToGo) / xp.pointsLevelNext) * 100);
+            const pointsFromLevel = xp.pointsLevelNext - xp.pointsToGo;
+            const pointsLevelNext = xp.pointsLevelNext;
+            const pointsToNext = xp.pointsToGo;
+            if (message.channel.permissionsFor(message.guild.me || "")?.has(Permissions.FLAGS.ATTACH_FILES)) {
+                const r = await fetch(`https://vacefron.nl/api/rankcard?username=${encodeURIComponent(target.user.tag)}&avatar=${encodeURIComponent(target.user.displayAvatarURL())}&currentxp=${xp.points}&nextlevelxp=${client.database.getCumulativePointsForLevel(xp.level + 1)}&previouslevelxp=${xp.pointsLevelNow}&level=${xp.level}&rank=${personal ? personal.rank : "undefined"}&custombg=1b3080&xpcolor=FaFaFa&isboosting=${target.premiumSince ? "true" : "false"}&circleavatar=true`);
+                if (r.status !== 200) {
+                    const j = await r.json();
+                    console.log(j)
+                    throw new Error(`VACEfron API Not OK: ${j.status} (status ${j.code})`);
                 }
-            });
+                const b = await r.buffer();
+                const att = new MessageAttachment(b);
+                await message.channel.send({ files: [att] });
+            } else {
+                await message.channel.send({
+                    embed: {
+                        color: await client.database.getColor("info"),
+                        description: `**${target.displayName}** has ${verbs[Math.floor(Math.random() * verbs.length)]} **${sym}** \`${xp.points}\` total\n**Level:** \`${xp.level}\`\n**${sym}** \`${pointsToNext}\` more is needed for level \`${xp.level + 1}\`\n\`${pointsFromLevel}\` / \`${pointsLevelNext}\` (\`${percentToNext}\`%)`,
+                    }
+                });
+            }
         } catch (error) {
             xlg.error(error);
             await client.specials?.sendError(message.channel);
