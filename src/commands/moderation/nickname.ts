97d24fc330c69750134a86fd8e0aa693b87f3418
Rooskie <polyversialmind@gmail.com>
2021-06-19 19:00:27 -0500

    fix(command): :art: better nickname perm check and fallback message + code quality
    Brings to the current code quality standard of this repo and checks to see whether the target is the owner (to make a more specific fallback message). Additionally: fixed a bug where if the nickname (after specifying the sender as the target) began with the id of the sender, that part of the nickname would be removed.

--- src/commands/moderation/nickname.ts
+++ src/commands/moderation/nickname.ts
@@ -1,24 +1,20 @@
-
-//import { getGuildSetting } from "../dbmanager";
 import { permLevels, getPermLevel } from "../../permissions";
 import { stringToMember } from "../../utils/parsers";
-import { Command } from "src/gm";
+import { Command, GuildMessageProps } from "src/gm";
 
-export const command: Command = {
+export const command: Command<GuildMessageProps> = {
     name: 'nickname',
     aliases: ['nick'],
-    usage: '[target member],<new nick>',
+    usage: '[target member] <new nick>',
     guildOnly: true,
-    description: 'set a member nickname',
+    description: {
+        short: "set a member nickname",
+        long: "Set the nickname of yourself or another member (if you have the necessary permissions)",
+    },
     args: true,
+    permissions: ["MANAGE_NICKNAMES"],
     async execute(client, message, args) {
         try {
-            if (!message.guild || !message.member) return;
-            /*let moderationEnabled = await getGuildSetting(message.guild, 'all_moderation');
-            if (!moderationEnabled[0] || moderationEnabled[0].value === 'disabled') {
-                return client.specials.sendModerationDisabled(message.channel);
-            }*/
-            
             let target = await stringToMember(message.guild, args[0], true, false, false);
             if (!target || !target.id) {
                 target = message.member;
@@ -29,54 +25,60 @@ export const command: Command = {
                     // now checking for moderation here because this means that someone elses name from the sender is being changed
                     const moderationEnabled = await client.database.getGuildSetting(message.guild, 'all_moderation');
                     if (!moderationEnabled || moderationEnabled.value === 'disabled') {
-                        return client.specials?.sendModerationDisabled(message.channel);
+                        await client.specials.sendModerationDisabled(message.channel);
+                        return;
                     }
                     // confirming that the author has the moderation privileges to use the command
                     const permLevel = await getPermLevel(message.member);
                     if (permLevel < permLevels.mod) {
-                        message.channel.send("Insufficient permissions.").catch(xlg.error);
+                        await message.channel.send("Insufficient permissions.").catch(xlg.error);
                         return;
                     }
                 }
             }
 
             // This is probably an unecessary check but I probably had a reason so I'll keep it
-            if (args[0] === message.member.id) args.shift();
+            // if (args[0] === message.member.id) args.shift();// looking at this later, this should never be true since the target is already parsed earlier
+            // ^ i am keeping this commented here so i will always be able to wonder why the hell i didn't delete this line when rewriting the commana
+            const a = args.join(" ")
 
             // Why would I want it to be changed manually
             //if (target.id == client.id) return message.channel.send('My nickname should be changed manually');
 
+            if (target.id === target.guild.ownerID) {
+                await client.specials.sendError(message.channel, `Because you are the server owner, I will never be able to change your nickname`);
+                return;
+            }
             // I would rather let this error
             //if (!target.manageable) return message.channel.send('I don't have the permissions to manage that target, I am outranked! 🚀 inbound');
 
             // Necessary check to prevent a needless error
             if (args.join(" ").length > 32) {
-                message.channel.send('Nicknames cannot be longer than 32 characters');
+                await message.channel.send('Nicknames cannot be longer than 32 characters');
                 return;
             }
 
             if (args.join(" ") == target?.nickname) {
-                message.channel.send(`\`${args.join(" ")}\` is already the target's nickname`);
+                await message.channel.send(`\`${a}\` is already the target's nickname`);
                 return;
             }
 
             if (args.join(" ").length < 1) {
-                message.channel.send("The new nickname had no length")
+                await message.channel.send("The new nickname had no length")
                 return;
             }
 
             try {
-                await target?.setNickname(args.join(" "), 'adjustment thru nick command');
-                await message.channel.send(`\\✅ Changed the nickname of \`${target.user.tag}\` to \`${target.nickname}\``)
+                await target.setNickname(args.join(" "), 'adjustment thru nick command');
+                await message.channel.send(`\\✅ Changed the nickname of \`${target.user.tag}\` to \`${target.displayName}\``)
             } catch (e) {
                 xlg.log(e.message);
-                await client.specials?.sendError(message.channel, `◍ Command Error:\n${e.message}`)
+                await client.specials.sendError(message.channel, `◍ Command Error:\n${e.message}`)
             }
         } catch (error) {
             xlg.error(error);
-            await client.specials?.sendError(message.channel);
+            await client.specials.sendError(message.channel);
             return false;
         }
     }
 }
-
