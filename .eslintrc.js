9d208eb5f1ae2da35254c0670526791c9cb01b32
Rooskie <polyversialmind@gmail.com>
2021-07-04 01:47:01 -0500

    chore(deps): :arrow_up: dependency upgrades while trying to fix typescript transpiling errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-{
+module.exports = {
     "env": {
-        "browser": true,
-        "commonjs": true,
         "es2021": true,
         "node": true
     },
@@ -11,11 +9,12 @@
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": 12
+        "ecmaVersion": 12,
+        "sourceType": "module"
     },
     "plugins": [
         "@typescript-eslint"
     ],
     "rules": {
     }
-}
+};
