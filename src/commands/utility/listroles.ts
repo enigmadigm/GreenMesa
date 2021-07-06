ef9f7027c94a9d32822d2cdde398b0e6899d0cce
Rooskie <polyversialmind@gmail.com>
2021-07-06 15:19:31 -0500

    fix(command): :sparkles: modify listroles for format and adding new flags

--- src/commands/utility/listroles.ts
+++ src/commands/utility/listroles.ts
@@ -1,6 +1,6 @@
 import { permLevels } from '../../permissions';
 import { Command, GuildMessageProps } from "src/gm";
-import { MessageEmbedOptions } from "discord.js";
+import { MessageEmbedOptions, Permissions } from "discord.js";
 import { PaginationExecutor } from "../../utils/pagination";
 const maxlen = 15;// maximum number of roles to be displayed at one time
 
@@ -9,19 +9,32 @@ export const command: Command<GuildMessageProps> = {
     aliases: ['lsroles'],
     description: {
         short: `display roles in the server`,
-        long: `Display the full list of roles in the server`,
+        long: `Display the full list of roles in the server. This command is restricted to trusted members (lvl 1 PA) so that users interested in raiding the server do not have easy access to view roles.`,
     },
+    flags: [
+        {
+            f: "e",
+            d: "display roles with mention anything perms",
+        },
+        {
+            f: "nonman",
+            d: "list only non-managed roles (bot roles)",
+        }
+    ],
     guildOnly: true,
     permLevel: permLevels.trustedMember,
-    async execute(client, message) {
+    async execute(client, message, args, flags) {
         try {
+            const everyoneMentionableFlag = flags.find(x => x.name === "e" && !x.value);
+            const nonManagedFlag = flags.find(x => x.name === "nonman" && !x.value);
             const roleArray = message.guild.roles.cache
                 .sort((roleA, roleB) => roleB.position - roleA.position)
-                .filter((x) => x.name !== "@everyone")
+                .filter((x) => x.name !== "@everyone" && (!everyoneMentionableFlag || (x.permissions.bitfield & Permissions.FLAGS.MENTION_EVERYONE) === Permissions.FLAGS.MENTION_EVERYONE) && (!nonManagedFlag || !x.managed))
                 .map((r, i, c) => {
                     const prefix = `${c.size}`.split(/./).map(() => "0").join("").slice(0, `${c.size}`.length - `${r.position}`.length);
                     return `\`${prefix}${r.position}\` ${r} *${r.id}*`;
                 });
+            const roleCount = roleArray.length;
             const roleOverflowArray: string[][] = [];
             let runs = 0;
             if (roleArray.length > maxlen || roleArray.join("\n").length > 1024) {
@@ -51,18 +64,17 @@ export const command: Command<GuildMessageProps> = {
                     },
                     description: `${page.join("\n") || '*none*'}`,
                     footer: {
-                        text: `Roles: ${message.guild.roles.cache.array().length}`
+                        text: `Roles: ${roleCount}${everyoneMentionableFlag ? " • Can Mention Everyone" : ""}`
                     }
                 };
                 pages.push(e);
             }
 
-            PaginationExecutor.createEmbed(message, pages);
+            await PaginationExecutor.createEmbed(message, pages);
         } catch (error) {
             xlg.error(error);
-            await client.specials?.sendError(message.channel);
+            await client.specials.sendError(message.channel);
             return false;
         }
     }
 }
-
